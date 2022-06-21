9a724f686ea67425137e976dc1726f819fd100fc
Roman <romanshliakhov@gmail.com>
2022-06-21 20:05:23 +0300

    added calculator left and right blocks

--- source/js/common.js
+++ source/js/common.js
@@ -1,3 +1,67 @@
+// Steps
+const stepsBtns = [...document.querySelectorAll('.calculate__types-item')];
+const stepsWrapper = document.querySelector('.calculate__steps-all');
+const cutrentForms = [...document.querySelectorAll(".calculate__steps-pressed")];
+let btnData;
+let active;
+let show;
+
+const itemAddClass = function(item,activeClass) {
+    item.classList.add(activeClass);
+}
+
+const itemRemoveClass = function(item,activeClass) {
+    item.classList.remove(activeClass);
+}
+
+const itemToggleClass = function(item,activeClass) {
+    item.classList.toggle(activeClass);
+}
+
+stepsBtns.map(function(stepsBtn) {
+    stepsBtn.addEventListener('click', function() {
+        active = 'active';
+        show = 'show';
+
+        btnData = this.dataset.choose;
+        cutrentForms.map(function(item){
+            item.dataset.form !== btnData ?
+            itemRemoveClass(item,active) :
+            itemAddClass(item,active);
+
+            let stepsPrev = item.querySelector('.calculate__steps-btn--back');
+
+            stepsPrev.onclick = function(e){
+                e.preventDefault();
+                itemRemoveClass(stepsWrapper,show);
+                itemRemoveClass(item,active);
+            };
+        });
+
+        itemAddClass(stepsWrapper,show);
+    });
+})
+
+
+
+
+// upload
+function upload() {
+    const input = document.querySelector('.calculate__upload-file');
+
+    const changeHandler = event => {
+        console.log(event);
+    }
+
+    input.addEventListener('change', changeHandler);
+
+}
+
+
+
+
+
+
 // burger
 let menuBtnOpen = document.querySelector('.mobile__header-btn--open');
 let menuBtnClose = document.querySelector('.mobile__header-btn--close');
@@ -347,40 +411,5 @@ if (tabsParr) {
   });
 }
 
-// Calculate Steps
-function stepForm () {
-    const steps = document.querySelectorAll('.calculate__steps-pressed');
-    const prevBtn = document.querySelector('.calculate__steps-btn--back');
-    const nextBtn = document.querySelector('.calculate__types-item');
-    const form = document.querySelector('.calculate__form');
-
-
-
-    form.addEventListener('submit', (e) => e.preventDefault());
-
-    let formStep = 0;
-
-    prevBtn.addEventListener('click', () => {
-        formStep--;
-        updateFormSteps();
-    });
-
-    nextBtn.addEventListener('click', () => {
-        formStep++;
-        updateFormSteps();
-    });
-
-    function updateFormSteps() {
-        steps.forEach(step => {
-            step.classList.contains('active') && step.classList.remove('active');
-        });
-
-        // steps[formStep].classList.add('active');
-    }
-
-    updateFormSteps();
-}
-
-stepForm();
 
 
