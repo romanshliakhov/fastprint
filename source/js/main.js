83ca9eae4b46756ed859f05694061f8680962565
Roman <romanshliakhov@gmail.com>
2022-07-01 16:05:00 +0300

    added adaptive for basket

--- source/js/main.js
+++ source/js/main.js
@@ -538,22 +538,22 @@ const modal = new Modal({
 });
 
 // scroll to
-// document.querySelectorAll('a.first__wrapp-order').forEach(link => {
-//   link.addEventListener('click', function(e) {
-//     e.preventDefault();
+document.querySelectorAll('a.first__wrapp-order').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
 
-//     const href = this.getAttribute('href').substring(1);
+    const href = this.getAttribute('href').substring(1);
 
-//     const scrollTarget = document.getElementById(href);
-//     const topOffset = 0;
-//     const elementPosition = scrollTarget.getBoundingClientRect().top;
-//     const offsetPosition = elementPosition - topOffset;
+    const scrollTarget = document.getElementById(href);
+    const topOffset = 0;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
 
-//     window.scrollBy({
-//       top: offsetPosition,
-//       behavior: 'smooth'
-//     })
-//   })
-// })
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth'
+    })
+  })
+})
 
 
