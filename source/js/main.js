5bc79c67d48f5bfe6dca3581725c0952cdd978b0
Roman <romanshliakhov@gmail.com>
2022-07-05 23:48:29 +0300

    fix overflow-x

--- source/js/main.js
+++ source/js/main.js
@@ -283,16 +283,16 @@ if (bannerItem) {
 }
 
 // Close timer
-const closeBtnTimer = document.querySelector('.calculate__shipping-close');
-const timerItem = document.querySelector('.calculate__shipping');
+// const closeBtnTimer = document.querySelector('.calculate__shipping-close');
+// const timerItem = document.querySelector('.calculate__shipping');
 
-const addClassHideTimer = function(button, body) {
-    button.onclick = () => body.classList.add('hide');
-}
+// const addClassHideTimer = function(button, body) {
+//     button.onclick = () => body.classList.add('hide');
+// }
 
-if (timerItem) {
-  addClassHideTimer(closeBtnTimer, timerItem);
-}
+// if (timerItem) {
+//   addClassHideTimer(closeBtnTimer, timerItem);
+// }
 
 
 // dropdown
