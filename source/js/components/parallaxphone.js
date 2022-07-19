cdf07aea1a34da52d6ee014d28392b1ef705d808
Roman <romanshliakhov@gmail.com>
2022-07-19 17:02:14 +0300

    remove parallax

--- source/js/components/parallaxphone.js
+++ source/js/components/parallaxphone.js
@@ -1,25 +1,25 @@
-if (window.boxercontainer) {
-  // Parallax phone
-  const boxer = boxercontainer.querySelector("img"),
-    boxerCenterX = boxer.offsetLeft + (boxer.offsetWidth / 2),
-    boxerCenterY = boxer.offsetTop + (boxer.offsetHeight / 2),
-    fluidboxer = window.matchMedia("(min-width: 726px)");
+// if (window.boxercontainer) {
+//   // Parallax phone
+//   const boxer = boxercontainer.querySelector("img"),
+//     boxerCenterX = boxer.offsetLeft + (boxer.offsetWidth / 2),
+//     boxerCenterY = boxer.offsetTop + (boxer.offsetHeight / 2),
+//     fluidboxer = window.matchMedia("(min-width: 726px)");
 
-  function getMousePos(xRef, yRef) {
-    let panelRect = boxercontainer.getBoundingClientRect();
-    return {
-      x: Math.floor(xRef - panelRect.left) / (panelRect.right - panelRect.left) * boxercontainer.offsetWidth,
-      y: Math.floor(yRef - panelRect.top) / (panelRect.bottom - panelRect.top) * boxercontainer.offsetHeight
-    };
-  }
+//   function getMousePos(xRef, yRef) {
+//     let panelRect = boxercontainer.getBoundingClientRect();
+//     return {
+//       x: Math.floor(xRef - panelRect.left) / (panelRect.right - panelRect.left) * boxercontainer.offsetWidth,
+//       y: Math.floor(yRef - panelRect.top) / (panelRect.bottom - panelRect.top) * boxercontainer.offsetHeight
+//     };
+//   }
 
-  document.body.addEventListener("mousemove", function (e) {
-    let mousePos = getMousePos(e.clientX, e.clientY),
-      distX = mousePos.x - boxerCenterX,
-      distY = mousePos.y - boxerCenterY;
-    if (Math.abs(distX) < 500 && distY < 200 && fluidboxer.matches) {
-      boxer.style.transform = "translate(" + (-1 * distX) / 20 + "px," + (-1 * distY) / 20 + "px)";
-      boxercontainer.style.backgroundPosition = `calc(50% + ${distX / 5}px) calc(50% + ${distY / 5}px)`;
-    }
-  })
-}
+//   document.body.addEventListener("mousemove", function (e) {
+//     let mousePos = getMousePos(e.clientX, e.clientY),
+//       distX = mousePos.x - boxerCenterX,
+//       distY = mousePos.y - boxerCenterY;
+//     if (Math.abs(distX) < 500 && distY < 200 && fluidboxer.matches) {
+//       boxer.style.transform = "translate(" + (-1 * distX) / 20 + "px," + (-1 * distY) / 20 + "px)";
+//       boxercontainer.style.backgroundPosition = `calc(50% + ${distX / 5}px) calc(50% + ${distY / 5}px)`;
+//     }
+//   })
+// }
